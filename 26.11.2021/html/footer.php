3be3281750b84e4762a9fd851ad6c17eeb97a813
Puchkova Anastasia <Boginia357@rambler.ru>
2021-11-29 01:53:36 +0500

    26.11.2021

---
+++ 26.11.2021/html/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/Styles.css">
+    <title>Title</title>
+</head>
+</html>
+<footer class="footer">Контакты:
+    <a href="https://www.instagram.com">Instagram</a>
+    <a href="https://www.vk.com">VK</a>
+    <a href="https://www.twitter.com"> Twitter</a>
+</footer>
+
+
